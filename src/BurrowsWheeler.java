eb50ab8f7176391c9c71045edb7a59a045f70bff
kronwerk <tetragon@mail.ru>
2014-12-30 13:22:47 +0300

    eee

--- src/BurrowsWheeler.java
+++ src/BurrowsWheeler.java
@@ -1,15 +1,24 @@
 public class BurrowsWheeler {
+    private static final int BYTE = 8;
+
     // apply Burrows-Wheeler encoding, reading from standard input and writing to standard output
     public static void encode() {
-        String input = BinaryStdIn.readString();
+        String input = "";
+        /*while (true) {
+            char end = BinaryStdIn.readChar();
+            if (end == '0')
+                break;
+            input += end;
+        }*/
+        input = BinaryStdIn.readString();
         CircularSuffixArray sfa = new CircularSuffixArray(input);
         for (int i = 0; i < input.length(); i++) {
             int j = sfa.index(i);
             if (j == 0) {
-                BinaryStdOut.write(i, 4);
+                BinaryStdOut.write(i, 4 * BYTE);
                 j = input.length();
             }
-            BinaryStdOut.write(input.charAt(j-1), 1);
+            BinaryStdOut.write(input.charAt(j-1), 1 * BYTE);
         }
         BinaryStdOut.close();
     }
@@ -22,6 +31,18 @@ public class BurrowsWheeler {
     // if args[0] is '-', apply Burrows-Wheeler encoding
     // if args[0] is '+', apply Burrows-Wheeler decoding
     public static void main(String[] args) {
-        BurrowsWheeler.encode();
+        if (args[0].equals("-"))
+        {
+            encode();
+        }
+        else
+        if (args[0].equals("+"))
+        {
+            decode();
+        }
+        else
+        {
+            throw new RuntimeException("Illegal command line argument");
+        }
     }
 }
